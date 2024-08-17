d1cc8e9642f5630c38b23a69422acc6261705a13
Shiney <shiney618@proton.me>
2024-08-17 21:56:44 +0300

    remove examples

--- examples/che6/pieces/mod.rs
+++
@@ -1,7 +0,0 @@
-mod color;
-mod sheet;
-mod r#type;
-
-pub use color::*;
-pub use r#type::*;
-pub use sheet::*;
--- examples/che6/pieces/sheet.rs
+++
@@ -1,6 +0,0 @@
-use super::PieceColor;
-use super::PieceType;
-use sww::media::Sheet;
-
-pub type PiecesSheetCoord = (PieceType, PieceColor);
-pub type PiecesSheet = Sheet<PiecesSheetCoord>;
