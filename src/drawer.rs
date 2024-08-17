592b35fec667679dfc169187d49941fe4973d4da
Shiney <shiney618@proton.me>
2024-08-17 20:02:07 +0300

    impover `Drawer`s

--- src/drawer.rs
+++ src/drawer.rs
@@ -13,88 +13,112 @@ impl<'e> DrawingInfo<'e> {
     pub fn new(rw: &'e RenderWindow<'e>, render_pass: &'e mut wgpu::RenderPass<'e>) -> Self {
         Self { rw, render_pass }
     }
-}
 
-enum DrawerInner<'e> {
-    DrawingInfo(Option<DrawingInfo<'e>>),
-    Mesh(Option<MeshDrawer<'e>>),
-}
-
-impl<'e> DrawerInner<'e> {
-    pub const fn new(drawing_info: DrawingInfo<'e>) -> Self {
-        Self::DrawingInfo(Some(drawing_info))
+    pub fn rw(&self) -> &'e RenderWindow<'e> {
+        self.rw
     }
 
-    pub fn mesh(&mut self) -> &mut MeshDrawer<'e> {
-        if !matches!(self, Self::Mesh(Some(_))) {
-            let drawing_info = self.flush_take_drawing_info();
-            *self = Self::Mesh(Some(MeshDrawer::new(drawing_info)));
+    pub fn render_pass(&'e mut self) -> &'e mut wgpu::RenderPass<'e> {
+        self.render_pass
+    }
+}
+
+pub struct Drawable<'e, T> {
+    drawing_info: &'e mut DrawingInfo<'e>,
+    value: T,
+}
+
+impl<'e, T> Drawable<'e, T> {
+    pub fn new(drawing_info: &'e mut DrawingInfo<'e>, value: T) -> Self {
+        Self {
+            drawing_info,
+            value,
+        }
+    }
+}
+
+enum ActiveDrawer<'e> {
+    Mesh(MeshDrawerInfo<'e>),
+    _1,
+}
+
+impl<'e> From<MeshDrawerInfo<'e>> for ActiveDrawer<'e> {
+    fn from(mesh: MeshDrawerInfo<'e>) -> Self {
+        Self::Mesh(mesh)
+    }
+}
+
+impl<'e> ActiveDrawer<'e> {
+    pub fn mesh(&mut self, rw: &'e RenderWindow<'e>) -> &mut MeshDrawerInfo<'e> {
+        if !matches!(self, Self::Mesh(_)) {
+            *self = Self::Mesh(MeshDrawerInfo::new(rw));
         }
 
-        let Self::Mesh(Some(mesh)) = self else {
+        let Self::Mesh(mesh) = self else {
             unreachable!()
         };
 
         mesh
     }
-
-    fn flush_take_drawing_info(&mut self) -> DrawingInfo<'e> {
-        match self {
-            DrawerInner::DrawingInfo(drawing_info) => drawing_info.take().unwrap(),
-
-            DrawerInner::Mesh(mesh_drawer) => {
-                let mut mesh_drawer = mesh_drawer.take().unwrap();
-                mesh_drawer.flush();
-                mesh_drawer.drawing_info
-            }
-        }
-    }
 }
 
-pub struct Drawer<'e>(DrawerInner<'e>);
+pub struct Drawer<'e> {
+    drawing_info: DrawingInfo<'e>,
+    active: Option<ActiveDrawer<'e>>,
+}
 
 impl<'e> Drawer<'e> {
     pub const fn new(drawing_info: DrawingInfo<'e>) -> Self {
-        Self(DrawerInner::new(drawing_info))
-    }
-
-    pub fn mesh(&mut self) -> &mut MeshDrawer<'e> {
-        self.0.mesh()
-    }
-}
-
-pub struct MeshDrawer<'e> {
-    drawing_info: DrawingInfo<'e>,
-    transforms: MutVecBuffer<'e, Transform>,
-    mesh_drawing_info: Option<&'e MeshDrawingInfo<'e>>,
-}
-
-impl<'e> MeshDrawer<'e> {
-    pub fn new(drawing_info: DrawingInfo<'e>) -> Self {
-        let rw = drawing_info.rw;
         Self {
             drawing_info,
-            transforms: MutVecBuffer::default_vertex(rw),
-            mesh_drawing_info: None,
+            active: None,
         }
     }
 
-    pub fn draw(&mut self, mesh_drawing_info: &'e MeshDrawingInfo, transform: Transform) {
+    pub fn mesh(&'e mut self) -> MeshDrawer<'e> {
+        let mesh = self
+            .active
+            .get_or_insert_with(|| MeshDrawerInfo::new(self.drawing_info.rw).into())
+            .mesh(self.drawing_info.rw);
+
+        Drawable::new(&mut self.drawing_info, mesh)
+    }
+}
+
+pub struct MeshDrawerInfo<'e> {
+    transforms: MutVecBuffer<'e, Transform>,
+    current_mesh: Option<&'e MeshDrawingInfo<'e>>,
+}
+
+impl<'e> MeshDrawerInfo<'e> {
+    pub fn new(rw: &'e RenderWindow) -> Self {
+        Self {
+            transforms: MutVecBuffer::default_vertex(rw),
+            current_mesh: None,
+        }
+    }
+}
+
+pub type MeshDrawer<'e> = Drawable<'e, &'e mut MeshDrawerInfo<'e>>;
+
+impl<'e> MeshDrawer<'e> {
+    pub fn draw(&mut self, mesh: &'e MeshDrawingInfo, transform: Transform) {
         if self
-            .mesh_drawing_info
-            .map(|self_mesh_drawing_info| self_mesh_drawing_info.mesh.id())
-            != Some(mesh_drawing_info.mesh.id())
+            .value
+            .current_mesh
+            .map(|current_mesh| current_mesh.mesh.id())
+            != Some(mesh.mesh.id())
         {
             self.flush();
         }
 
-        self.mesh_drawing_info = Some(mesh_drawing_info);
-        self.transforms.push(transform);
+        self.value.current_mesh = Some(mesh);
+        self.value.transforms.push(transform);
     }
 
     fn flush(&mut self) {
-        if let Some(mesh_drawing_info) = self.mesh_drawing_info {
-            mesh_drawing_info.draw(self.drawing_info.render_pass, &mut self.transforms);
+        if let Some(mesh_drawing_info) = self.value.current_mesh {
+            mesh_drawing_info.draw(self.drawing_info.render_pass, &mut self.value.transforms);
         }
     }
 }
