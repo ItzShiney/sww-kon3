fcfab73d6bef8ad323fecffa30d6a1ecaa29d794
Shiney <shiney618@proton.me>
2024-08-20 14:02:00 +0300

    add some constants and methods to `Color`

--- src/color.rs
+++ src/color.rs
@@ -17,10 +17,13 @@ impl Default for Color {
 
 impl Color {
     pub const WHITE: Self = Self::splat(1.);
+    pub const BLACK: Self = Self::splat(0.);
     pub const RED: Self = Self::new_rgb(1., 0., 0.);
     pub const GREEN: Self = Self::new_rgb(0., 1., 0.);
     pub const BLUE: Self = Self::new_rgb(0., 0., 1.);
-    pub const BLACK: Self = Self::splat(0.);
+    pub const YELLOW: Self = Self::new_rgb(1., 1., 0.);
+    pub const MAGENTA: Self = Self::new_rgb(1., 0., 1.);
+    pub const CYAN: Self = Self::new_rgb(0., 1., 1.);
     pub const TRANSPARENT: Self = Self::splat_a(0.);
 
     pub const fn new_rgba(r: f32, g: f32, b: f32, a: f32) -> Self {
@@ -45,6 +48,22 @@ impl Color {
 
     pub const fn to_vec4(self) -> Vec4 {
         Vec4::from_array(self.to_array())
+    }
+
+    pub const fn with_r(self, r: f32) -> Self {
+        Self::new_rgba(r, self.g, self.b, self.a)
+    }
+
+    pub const fn with_g(self, g: f32) -> Self {
+        Self::new_rgba(self.r, g, self.b, self.a)
+    }
+
+    pub const fn with_b(self, b: f32) -> Self {
+        Self::new_rgba(self.r, self.g, b, self.a)
+    }
+
+    pub const fn with_a(self, a: f32) -> Self {
+        Self::new_rgba(self.r, self.g, self.b, a)
     }
 }
 
